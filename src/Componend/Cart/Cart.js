49bafa53b8e3fc0e045c86f4d06ed1c91bd5b507
majharul224 <majharulislam2225@gmail.com>
2022-03-24 22:39:41 +0600

    add new commit

--- src/Componend/Cart/Cart.js
+++ src/Componend/Cart/Cart.js
@@ -3,19 +3,23 @@ import './Cart.css'
 // const Cart = ({cart}) => {অথবা
 const Cart = (props) => {
     const {cart} = props;
-   // console.log(cart)
+    console.log(cart)
    let total = 0;
+   let shipping = 0;
    for(const product of cart){
-         total = total + product.price;
+         total = total + product.price ;
+         shipping = shipping + product.shipping ;
    }
+   const tax = parseFloat((total * 0.1).toFixed(2));
+   const grandTotal = total + shipping + tax ;
     return (
         <div className='cart'>
                <h4>Order Summary</h4>
                 <p>Selected Items : {cart.length}</p>
                 <p>Total Price:{total} </p>
-                <p>Total Shipping Charge:</p>
-                <p>Tax: </p>
-                <h4>Grand Total: </h4>
+                <p>Total Shipping Charge: {shipping} </p>
+                <p>Tax: {tax} </p>
+                <h4>Grand Total: {grandTotal} </h4>
         </div>
     );
 };
